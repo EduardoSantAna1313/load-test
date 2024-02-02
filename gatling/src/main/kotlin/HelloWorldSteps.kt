cedf3aed48c7685ae33828fc05ffc5b6e09874c3
Eduardo <eduardosantana1313@gmail.com>
2024-02-02 11:54:40 -0300

    add input wf

--- gatling/src/main/kotlin/HelloWorldSteps.kt
+++ gatling/src/main/kotlin/HelloWorldSteps.kt
@@ -11,10 +11,23 @@ class HelloWorldSteps {
             atOnceUsers(1)
         )
 
+        private fun ntests(n: Int): List<OpenInjectionStep> = listOf(
+            atOnceUsers(n)
+        )
+
         private fun stressTest(): List<OpenInjectionStep> = listOf(
             constantUsersPerSec(2.0).during(10), // Aquecendo a aplicação
             constantUsersPerSec(2.0).during(10).randomized(), // preparando
             rampUsersPerSec(6.0).to(300.0).during(30) // THIS IS SPARTAAA!!!
+        )
+
+        private fun basic(): List<OpenInjectionStep> = listOf(
+            constantUsersPerSec(1.0).during(5), // Aquecendo a aplicação
+            nothingFor(2), // wait
+
+            constantUsersPerSec(2.0).during(10).randomized(), // preparando
+            nothingFor(2), // wait
+            rampUsersPerSec(2.0).to(5.0).during(10) // test
         )
 
         fun defaultSteps(): List<OpenInjectionStep> {
@@ -22,7 +35,13 @@ class HelloWorldSteps {
             if ("SINGLE" === profile)
                 return singleCall()
 
-            return stressTest()
+            if ("STRESS" === profile)
+                return stressTest()
+
+            if ("BASIC" === profile)
+                return basic()
+
+            return ntests(10)
         }
 
     }
